16530d61700272754b24f9056779b4dfb1176c84
wonsox <82097943+wonsox@users.noreply.github.com>
2021-11-24 14:55:43 +0900

    Add files via upload

--- movingkey_2.c
+++ movingkey_2.c
@@ -6,14 +6,30 @@
 #include <time.h>
 #include "map.h"
 
+//프로그램 실행 제어변수
+int RUN = 1;
+
+
 int premise = 1;//스테이지
+int gameover = 0;
 int direction = 1;
 int totalmap[30][30] = { {0}, {0} };
+
+
+int cnt_ai = 0;
 //플레이어 위치
 int Px = 15, Py =15;
+//인공지능 관련
 int Ax = 7, Ay = 7;
+int confirm_U = 0;
+int confirm_D = 0;
+int confirm_R = 0;
+int confirm_L = 0;
+//
 int Bpx, Bpy;
+
 int BART_CLEAR_CNT = 0;
+int AI_BART_CLEAR_CNT = 0;
 void change_color(int color_number);
 void AI();
 void gotoxy(int x, int y);
@@ -65,6 +81,7 @@ void double_buff() {
 			if (backbuff[i][j] != frontbuff[i][j]) {
 				if (backbuff[i][j] == 2) {
 					gotoxy(j * 2, i);
+					change_color(5);
 					printf("◎");
 				}else if (backbuff[i][j] == 0) {
 					gotoxy(j * 2, i);
@@ -72,14 +89,17 @@ void double_buff() {
 				}
 				else if (backbuff[i][j] == 3) {
 					gotoxy(j * 2, i);
+					change_color(2);
 					printf("♨");
 				}
 				else if (backbuff[i][j] == 5) {
 					gotoxy(j * 2, i);
+					change_color(15);
 					printf("☆");
 				}
 				else if (backbuff[i][j] == 6) {
 					gotoxy(j * 2, i);
+					change_color(12);
 					printf("★");
 				}
 			}
@@ -272,30 +292,44 @@ void initialize() {
 }
 
 int bart_detect() {
-	BART_CLEAR_CNT = 0;
+	
 	for (int i = 0; i < THEIGHT; i++) {
 
 		for (int j = 0; j < TWIDTH; j++) {
 			if (totalmap[i][j] == 3) {
 				BART_CLEAR_CNT++;
-				if (BART_CLEAR_CNT == 50) {//임시함수, 봇과 대결하여 더많을시에 스테이지 클리어로 개조할것-
-					premise = 0;
-					
-				}
+				
 			}
+			if (totalmap[i][j] == 6) {
+				AI_BART_CLEAR_CNT++;
+				
+			}
+
+			
+
 		}
+	}
+	if (BART_CLEAR_CNT > AI_BART_CLEAR_CNT) {
+		premise = 0;
+		
+	}
+	else {
+		gameover = 1;
+		premise = 0;
 	}
 	
 }
 
 void timer() {
+	
 	clock_t start = clock();
 	clock_t end = clock();
 	int time = ((int)end - start / CLOCKS_PER_SEC) / 1000; //초단위 변환
 
 
-	if (time == 100) {
-		premise = 0;
+	if (time == 5) {
+		bart_detect();
+		
 	}
 
 		
@@ -305,88 +339,129 @@ void timer() {
 void AI() {
 	int tmpx = 0;
 	int tmpy = 0;
+	
 	int select = randomize();
 	int lazy = randomize();
 	tmpx = Ax;
 	tmpy = Ay;
-	if (lazy < 4) {
-		lazy = 0;
-	}
-	else if (lazy >= 4) {
-		lazy = 1;
-	}
-	if (lazy == 1) {
 
-	}
-	if (totalmap[Ay][Ax] != 1 ) {
-		if (direction == 1) {//위 1
-			--Ay;
+	if (cnt_ai % 3000 == 0) {
+		if (totalmap[Ay][Ax] != 1) {
+			if (direction == 1) {//위 1
+				--Ay;
+			}
+			else if (direction == 2) {//아래 2
+				++Ay;
+			}
+			else if (direction == 3) {//왼 3
+				--Ax;
+			}
+			else if (direction == 4) {//오른 4
+				++Ax;
+			}
+
 		}
-		else if (direction == 2) {//아래 2
-			++Ay;
-		}
-		else if (direction == 3) {//왼 3
-			--Ax;
-		}
-		else if (direction == 4) {//오른 4
-			++Ax;
+		//위: 1, 아래: 2, 왼: 3, 오: 4
+		if (totalmap[Ay][Ax] == 1) {
+			if (select <= 2 && confirm_U != 1) {//위 1
+				
+				direction = 1;
+				//재설정
+				confirm_U = 1;
+				confirm_D = 0;
+				confirm_R = 0;
+				confirm_L = 0;
+			}
+			else if ((select > 2 && select <= 4) && confirm_D != 1) {//아래 2
+				direction = 2;
+				//재설정
+				confirm_U = 0;
+				confirm_D = 1;
+				confirm_R = 0;
+				confirm_L = 0;
+			}
+			else if ((select > 4 && select <= 6) && confirm_L != 1) {//왼 3
+				direction = 3;
+				//재설정
+				confirm_U = 0;
+				confirm_D = 0;
+				confirm_R = 0;
+				confirm_L = 1;
+			}
+			else if ((select > 6 && select <= 8) && confirm_R != 1) {//오른 4
+				direction = 4;
+				//재설정
+				confirm_U = 0;
+				confirm_D = 0;
+				confirm_R = 0;
+				confirm_L = 1;
+			}
+			Ax = tmpx;
+			Ay = tmpy;
 		}
 
-	}
-	//위: 1, 아래: 2, 왼: 3, 오: 4
-	if (totalmap[Ay][Ax] == 1) {
-		if (select <= 2) {//위 1
-			direction = 1;
-		}
-		else if (select > 2 && select <= 4) {//아래 2
-			direction = 2;
-		}
-		else if (select > 4 && select <= 6) {//왼 3
-			direction = 3;
-		}
-		else if (select > 6 && select <= 8) {//오른 4
-			direction = 4;
-		}
-		Ax = tmpx;
-		Ay = tmpy;
 	}
 	
 	/*Sleep(500);*/
 	//이걸쓰니까 플레이어의 입력이 많이 느려진다 이것말고 딴걸 써야하나
 	totalmap[tmpy][tmpx] = 6;
 	totalmap[Ay][Ax] = 5;
+	cnt_ai++;
 }
 
 int main() {
-	
-	//initialize
-	initialize();
-	//------------------
-	//아마 while문
-	
-	TMP(totalmap, 30, 30);//이것도 임시... 초기화에 넣어야하나? 일단은 움직임
-	
-	while (premise) {
-		{
-			bart_detect();//이함수를 카운터 함수에 넣고 일정시간이 지나면 스테이지 클리어로 진
-			timer();
-			db_saving(frontbuff);
-			
-			/*TMP(frontbuff, TWIDTH, THEIGHT);*/
-			//프론트버퍼 상태저장
-			AI();
-			Player();//이상없음 작동잘됨
-			//모든 처리가 끝난후 백버퍼 상태저장 
-			db_saving(backbuff);
-			/*TMP(backbuff, TWIDTH, THEIGHT);*/
-			double_buff();//불완성 강의참조요함
-			/*TMP(totalmap, TWIDTH, THEIGHT);*///이거 백버퍼문제다
-			//system("cls");//이건 임시
+	//프로그램의 실행 제어
+	while (RUN) {
+		//initialize
+		initialize();
+		//------------------
+		//아마 while문
+
+		TMP(totalmap, 30, 30);//이것도 임시... 초기화에 넣어야하나? 일단은 움직임
+
+		while (premise) {
+			{
+
+				timer();
+				db_saving(frontbuff);
+
+				/*TMP(frontbuff, TWIDTH, THEIGHT);*/
+				//프론트버퍼 상태저장
+				AI();
+				Player();//이상없음 작동잘됨
+				//모든 처리가 끝난후 백버퍼 상태저장 
+				db_saving(backbuff);
+				/*TMP(backbuff, TWIDTH, THEIGHT);*/
+				double_buff();//불완성 강의참조요함
+				/*TMP(totalmap, TWIDTH, THEIGHT);*///이거 백버퍼문제다
+				//system("cls");//이건 임시
+
+				//시간이 끝난후 플레이어의 흔적이 더많을때
+
+			}
 		}
+		gotoxy(31, 31);
+		printf("\n");
+		change_color(11);
+		if (gameover == 1) {
+			printf("게임오버\n");
+			Sleep(3000);
+			premise = 1;
+			
+		}
+		printf("스테이지 클리어\n");
+		printf("봇의 흔적 %d개\n", AI_BART_CLEAR_CNT);
+		printf("사람의 흔적 %d개\n", BART_CLEAR_CNT);
+		Sleep(3000);
+		premise = 1;
+		BART_CLEAR_CNT = 0;
+		AI_BART_CLEAR_CNT = 0;
+		
+		//일단 기본적인 틀은 만들어졌다
+		//한개의 씬 끝
+		system("cls");
+
 	}
-	printf("스테이지 클리어");
-	//일단 기본적인 틀은 만들어졌다
-	//한개의 씬 끝
 
 };
 //*현재 비디오 컨트롤러가 읽고 있는 버퍼를 프론트 버퍼로 정의,

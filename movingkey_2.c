a40ac169b1b01a8244b0d9675cc5beadba3d3070
wonsox <82097943+wonsox@users.noreply.github.com>
2021-11-24 12:59:46 +0900

    Add files via upload

--- movingkey_2.c
+++ movingkey_2.c
@@ -7,13 +7,15 @@
 #include "map.h"
 
 int premise = 1;//스테이지
-
+int direction = 1;
 int totalmap[30][30] = { {0}, {0} };
 //플레이어 위치
 int Px = 15, Py =15;
+int Ax = 7, Ay = 7;
 int Bpx, Bpy;
 int BART_CLEAR_CNT = 0;
 void change_color(int color_number);
+void AI();
 void gotoxy(int x, int y);
 void Pmove();
 void Player();
@@ -26,7 +28,6 @@ void R_Dcraft();
 void R_Ucraft();
 
 void TMP(int map[][30], int height, int width);
-
 
 void baseC(int map[][WIDTH], int Lheight, int Hheight, int Lwidth, int Hwidth) {//int로 바꾼다
 	//일정구간을 최종 맵저장소에 저장
@@ -72,6 +73,14 @@ void double_buff() {
 				else if (backbuff[i][j] == 3) {
 					gotoxy(j * 2, i);
 					printf("♨");
+				}
+				else if (backbuff[i][j] == 5) {
+					gotoxy(j * 2, i);
+					printf("☆");
+				}
+				else if (backbuff[i][j] == 6) {
+					gotoxy(j * 2, i);
+					printf("★");
 				}
 			}
 		}
@@ -178,7 +187,6 @@ void TMP(int map[][30], int height, int width) {//int로 바꾼다
 	}
 }
 
-
 void change_color(int color_number) //콘솔 출력색상 나타내는 함수
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_number);
@@ -195,17 +203,13 @@ void gotoxy(int x, int y)
 void Player() {
 	//{위,아래,왼,오}
 
-
 	Pmove();
-	
 }
-void collision(int direction[]) {
-	
-}
+
 void Pmove() {
 	int tmpx = Px;
 	int tmpy = Py;
-	
+	int direction = randomize();
 	totalmap[Py][Px] = 2;
 	
 	//이동
@@ -217,10 +221,10 @@ void Pmove() {
 			if (totalmap[Py][Px] == 1) {
 				Py = tmpy;
 			}
-			
+
 			break;
 		case DOWN:
-			
+
 			Py += 1;
 			if (totalmap[Py][Px] == 1) {
 				Py = tmpy;
@@ -231,19 +235,18 @@ void Pmove() {
 			if (totalmap[Py][Px] == 1) {
 				Px = tmpx;
 			}
-			
+
 			break;
 		case RIGHT:
 			Px += 1;
 			if (totalmap[Py][Px] == 1) {
 				Px = tmpx;
 			}
-			
+
 			break;
 		}
 		totalmap[tmpy][tmpx] = 3;
 	}
-	
 }
 //키입력에 따른 움직임
 
@@ -275,7 +278,7 @@ int bart_detect() {
 		for (int j = 0; j < TWIDTH; j++) {
 			if (totalmap[i][j] == 3) {
 				BART_CLEAR_CNT++;
-				if (BART_CLEAR_CNT == 10) {//임시함수, 봇과 대결하여 더많을시에 스테이지 클리어로 개조할것-
+				if (BART_CLEAR_CNT == 50) {//임시함수, 봇과 대결하여 더많을시에 스테이지 클리어로 개조할것-
 					premise = 0;
 					
 				}
@@ -291,7 +294,7 @@ void timer() {
 	int time = ((int)end - start / CLOCKS_PER_SEC) / 1000; //초단위 변환
 
 
-	if (time == 10) {
+	if (time == 100) {
 		premise = 0;
 	}
 
@@ -299,7 +302,60 @@ void timer() {
 	
 }
 
+void AI() {
+	int tmpx = 0;
+	int tmpy = 0;
+	int select = randomize();
+	int lazy = randomize();
+	tmpx = Ax;
+	tmpy = Ay;
+	if (lazy < 4) {
+		lazy = 0;
+	}
+	else if (lazy >= 4) {
+		lazy = 1;
+	}
+	if (lazy == 1) {
 
+	}
+	if (totalmap[Ay][Ax] != 1 ) {
+		if (direction == 1) {//위 1
+			--Ay;
+		}
+		else if (direction == 2) {//아래 2
+			++Ay;
+		}
+		else if (direction == 3) {//왼 3
+			--Ax;
+		}
+		else if (direction == 4) {//오른 4
+			++Ax;
+		}
+
+	}
+	//위: 1, 아래: 2, 왼: 3, 오: 4
+	if (totalmap[Ay][Ax] == 1) {
+		if (select <= 2) {//위 1
+			direction = 1;
+		}
+		else if (select > 2 && select <= 4) {//아래 2
+			direction = 2;
+		}
+		else if (select > 4 && select <= 6) {//왼 3
+			direction = 3;
+		}
+		else if (select > 6 && select <= 8) {//오른 4
+			direction = 4;
+		}
+		Ax = tmpx;
+		Ay = tmpy;
+	}
+	
+	/*Sleep(500);*/
+	//이걸쓰니까 플레이어의 입력이 많이 느려진다 이것말고 딴걸 써야하나
+	totalmap[tmpy][tmpx] = 6;
+	totalmap[Ay][Ax] = 5;
+}
 
 int main() {
 	
@@ -307,7 +363,7 @@ int main() {
 	initialize();
 	//------------------
 	//아마 while문
-	Player();
+	
 	TMP(totalmap, 30, 30);//이것도 임시... 초기화에 넣어야하나? 일단은 움직임
 	
 	while (premise) {
@@ -315,8 +371,10 @@ int main() {
 			bart_detect();//이함수를 카운터 함수에 넣고 일정시간이 지나면 스테이지 클리어로 진
 			timer();
 			db_saving(frontbuff);
+			
 			/*TMP(frontbuff, TWIDTH, THEIGHT);*/
 			//프론트버퍼 상태저장
+			AI();
 			Player();//이상없음 작동잘됨
 			//모든 처리가 끝난후 백버퍼 상태저장 
 			db_saving(backbuff);
